bea40f20a9dfa43ada7b0cfe9f64b74cda2689d2
kenty - 254759111854 <111528565+Mahmoudshee@users.noreply.github.com>
2026-02-25 12:20:45 +0300

    Delete Digitech Python directory

--- Digitech Python/day1.py
+++
@@ -1,7 +0,0 @@
-# my_name is kenty
-
-# my_name = "this is emy's bottle"
-# print(my_name)
-
-name = input("what is your name?: ")
-print(name)
--- Digitech Python/day6.py
+++
@@ -1,7 +0,0 @@
-a = 10
-b = 8
-
-calc = a ^ b
-binary = bin(calc)
-
-print(calc, "and it's binary is", binary)
